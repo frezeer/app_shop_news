c6143ba57f724dce3c0228699748e16864652979
frezeer <frezee13@hotmail.com>
2020-08-02 21:56:02 -0500

    Se crean los seeders de Poductos y Usuarios Factory

--- app/Product.php
+++ app/Product.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App;
+use App\Product;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
     //
+   
 }
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        User::create([
+        	'name' => 'Sergio',		
+            'email' => 'frezee13@gmail.com',
+            'password' => bcrypt ('erika#$18')  
+        ]);
+    }
+}
